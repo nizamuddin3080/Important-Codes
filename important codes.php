c71b28b91ad4d2c6711d1affc7c84a46c53dc368
Nizam Uddin khondokar <mdnizamuddin3080@gmail.com>
2023-04-11 08:40:02 +0600

    Update important codes.php

--- important codes.php
+++ important codes.php
@@ -1,4 +1,10 @@
+<!-- Redirect to http to https -->
+
+
 RewriteEngine On
 RewriteCond %{HTTPS} !=on
 RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301,NE]
 Header always set Content-Security-Policy "upgrade-insecure-requests;"
+
+
+
